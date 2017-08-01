86294ab7d00b7c9e1a44ec3f6d37d5cd4a793758
hisbmitchell <heathermitchell.ottawa@gmail.com>
2017-08-01 15:52:40 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var myDiv;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  myDiv = createDiv(<iframe src="https://player.vimeo.com/video/227977504" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-<p><a href="https://vimeo.com/227977504">pendulum_document</a> from <a href="https://vimeo.com/user7406603">Heather Mitchell</a> on <a href="https://vimeo.com">Vimeo</a>.</p>);
+  myDiv = createDiv([<iframe src="https://player.vimeo.com/video/227977504" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+<p><a href="https://vimeo.com/227977504">pendulum_document</a> from <a href="https://vimeo.com/user7406603">Heather Mitchell</a> on <a href="https://vimeo.com">Vimeo</a>.</p>]);
   
 }
 
